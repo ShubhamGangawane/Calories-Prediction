a3298c560685679c7649c3191b9dc62c64e49de1
Shubham Gangawane <85383832+ShubhamGangawane@users.noreply.github.com>
2021-12-13 11:10:36 +0530

    .py file

---
+++ main.py
@@ -0,0 +1,12 @@
+import streamlit as st
+from Web_Page import Page
+from ExplorePage import visualization
+
+
+mains = st.sidebar.selectbox("Prediction or visualization", ("Prediction","visualization"))
+
+if mains=="Prediction":
+    Page()
+else:
+    visualization()
+
